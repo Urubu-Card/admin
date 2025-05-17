c53f19354d1bfe390301889e6f5bfebac359b948
Urubu-Card <eduardolegalsans@gmail.com>
2025-05-17 11:44:23 -0300

    Update adiconarUsuariosStreamLit.py

--- adiconarUsuariosStreamLit.py
+++ adiconarUsuariosStreamLit.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 st.set_page_config(page_icon="❗",page_title="Adiconar Usuarios : ",layout="centered",)
 
-st.session_state = True
+
 
 
 st.title("Gerenciador de Tarefas : ")
